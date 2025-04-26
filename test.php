c9540fbb4535264d4320e4a7d6b6b43b78f5751c
ThatLukeDev <luke@l2j.co.uk>
2025-04-26 22:39:40 +0100

    problem in client-server comm

--- test.php
+++ test.php
@@ -2,34 +2,38 @@
 
 require_once "rlwe.php";
 
-// client 1
-$privKey = genPrivateRLWE($privSizeRLWE, $modulusRLWE);
-$pubKey = genPublicRLWE($privKey, $pubSizeRLWE, $modulusRLWE, $errorRLWE);
+session_start();
 
-// client 2
-$val = random_int(0, 255);
-$message = encodeByteRLWE($pubKey, $samplesRLWE, $modulusRLWE, $val);
-echo "Message: {$val}<br>";
+$_SESSION["privKey"] = genPrivateRLWE($privSizeRLWE, $modulusRLWE);
+$_SESSION["pubKey"] = genPublicRLWE($_SESSION["privKey"], $pubSizeRLWE, $modulusRLWE, $errorRLWE);
 
-// client 1
-$recieved = decodeByteRLWE($privKey, $message, $modulusRLWE);
-echo "Recieved: {$recieved}<br>";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$_SESSION["recieved"] = decodeByteRLWE($_SESSION["privKey"], json_decode(base64_decode($_GET["val"])), $modulusRLWE);
+	echo $_SESSION["recieved"];
+	echo "<non-post-start-split-header>";
+}
 
 ?>
 
 <input id="secret" placeholder="Input text here">
 
 <script src="rlwe-func.js"></script>
+<script> var pubKey = JSON.parse(atob("<?php echo base64_encode(json_encode($_SESSION["pubKey"])); ?>")); </script>
 <script>
 
-let privKey = autogenPrivateRLWE();
-let pubKey = autogenPublicRLWE(privKey);
-
-let val = Math.floor(Math.random() * 256);
+let val = 0;//Math.floor(Math.random() * 256);
 let encoded = autoencodeByteRLWE(pubKey, val);
-let decoded = autodecodeByteRLWE(privKey, encoded);
 
-document.querySelector("body").innerHTML += `<br>Sent ${val}`;
-document.querySelector("body").innerHTML += `<br>Recieved ${decoded}`;
+document.querySelector("body").innerHTML += `<br>1: ${val.toString(2).padStart(8, "0")}`;
+
+let xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = () => {
+	let response = xhttp.responseText.split("<non-post-start-split-header>")[0];
+	if (response) {
+		document.querySelector("body").innerHTML += `<br>2: ${parseInt(response).toString(2).padStart(8, "0")}`;
+	}
+};
+xhttp.open("POST", "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"+`?val=${btoa(JSON.stringify(encoded))}`, true);
+xhttp.send();
 
 </script>
