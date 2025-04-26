770834d6e918ffd5d4609d09ed0e03cf3c93ba31
ThatLukeDev <luke@l2j.co.uk>
2025-04-27 00:12:19 +0100

    polynomial multiplication and addition and

--- rlwe.php
+++ rlwe.php
@@ -1,3 +1,51 @@
 <?php
 
+function polyAdd($a, $b) {
+	$out = [];
+	$len = max(count($a), count($b));
+	for ($i = 0; $i < $len; $i++) {
+		$out[$i] = $a[$i] + $b[$i];
+	}
+
+	return $out;
+}
+
+function polySub($a, $b) {
+	$out = [];
+	$len = max(count($a), count($b));
+	for ($i = 0; $i < $len; $i++) {
+		$out[$i] = $a[$i] + $b[$i];
+	}
+
+	return $out;
+}
+
+function polyMul($a, $b) {
+	$out = [];
+
+	for ($i = 0; $i < count($a); $i++) {
+		for ($j = 0; $j < count($b); $j++) {
+			$out[$i + $j] += $a[$i] * $b[$j];
+		}
+	}
+
+	return $out;
+}
+
+?>
+
+<?php
+
+function polyDisplay($a) {
+	for ($i = 0; $i < count($a); $i++) {
+		echo "{$a[$i]} x^{$i}&nbsp; &nbsp; ";
+	}
+}
+
+?>
+
+<?php
+
+polyDisplay(polyMul([1, 2], [1, 3]));
+
 ?>
