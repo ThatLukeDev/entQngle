46aa4e828dec92eb72e9f7678b3a72abb34d46ea
ThatLukeDev <luke@l2j.co.uk>
2025-04-26 22:39:39 +0100

    users table

--- config.php
+++ config.php
@@ -8,4 +8,12 @@ if ($mysqli->connect_error) {
 	die("Could not connect to SQL Server: " . $mysqli->connect_error);
 }
 
+mysqli_select_db($mysqli, SQLDBNAME);
+
+mysqli_query($mysqli, "create table if not exists users(
+	username varchar(20),
+	password varchar(255),
+	PRIMARY KEY (username)
+);");
+
 ?>
