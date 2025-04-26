e8f312dcc153554de50437dd171bfe3c6e244bd2
ThatLukeDev <luke@l2j.co.uk>
2025-04-26 22:39:40 +0100

    moved key one down so easier to see

--- keytest.php
+++ keytest.php
@@ -9,8 +9,8 @@ session_start();
 autosessionRLWE();
 
 ?>
-last recieved (1 behind): <?php echo base64_encode(join(array_map("chr", $_SESSION["key"]))); ?>
 <div id="recieve">client recieved: </div>
+last recieved (1 behind): <?php echo base64_encode(join(array_map("chr", $_SESSION["key"]))); ?>
 <script>
 document.querySelector("#recieve").innerHTML += btoa(String.fromCharCode.apply(null, new Uint8Array(key)));
 </script>
