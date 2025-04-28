b181b2cc0a49d891467b103d5a135d6d4ebd2bae
ThatLukeDev <luke@l2j.co.uk>
2025-04-28 08:41:21 +0100

    poly mod seems to be working

--- rlwe.php
+++ rlwe.php
@@ -14,7 +14,7 @@ function polySub($a, $b) {
 	$out = [];
 	$len = max(count($a), count($b));
 	for ($i = 0; $i < $len; $i++) {
-		$out[$i] = $a[$i] + $b[$i];
+		$out[$i] = $a[$i] - $b[$i];
 	}
 
 	return $out;
@@ -49,6 +49,42 @@ function polyTrim($a) {
 	return $out;
 }
 
+function polyPower($a, $b) {
+	$out = [];
+
+	for ($i = 0; $i < $b; $i++) {
+		$out[$i] = 0;
+	}
+
+	for ($i = 0; $i < count($a); $i++) {
+		$out[count($out)] = $a[$i];
+	}
+
+	return $out;
+}
+
+function polyDegree($a) {
+	return count(polyTrim($a)) - 1;
+}
+
+function polyMod($eqn, $div) {
+	$equation = polyTrim($eqn);
+	$divisor = polyTrim($div);
+	$divisorDegree = polyDegree($divisor);
+	$divisorSignificant = $divisor[$divisorDegree];
+
+	$modulus = $equation;
+
+	for ($difference = polyDegree($equation) - $divisorDegree; $difference >= 0; $difference--) {
+		$shortDivisor = polyPower($divisor, $difference);
+		$shortQuotient = $modulus[$difference + $divisorDegree] / $divisorSignificant;
+
+		$modulus = polySub($modulus, polyMul($shortDivisor, [$shortQuotient]));
+	}
+
+	return polyTrim($modulus);
+}
+
 ?>
 
 <?php
@@ -57,12 +93,14 @@ function polyDisplay($a) {
 	for ($i = 0; $i < count($a); $i++) {
 		echo "{$a[$i]} x^{$i}&nbsp; &nbsp; ";
 	}
+	echo "<br>";
 }
 
 ?>
 
 <?php
 
-polyDisplay(polyTrim([1, 2, 4, 0, 2, 0, 0]));
+polyDisplay(polyMod([1, 2, 1], [1, 1]));
+polyDisplay(polyMod([-4, 0, -2, 1], [-3, 1]));
 
 ?>
