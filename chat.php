ac3c53f1e7cf6bbe1b0e5b4a77265d5e62346897
ThatLukeDev <luke@l2j.co.uk>
2025-06-04 08:24:04 +0100

    non working key fetch2

--- chat.php
+++ chat.php
@@ -1,7 +1,10 @@
 <?php
 
-require_once "pqkx.php";
+require_once "config.php";
+
 session_start();
+
+require_once "pqkx.php";
 
 ?>
 
@@ -11,6 +14,18 @@ $user = "";
 
 if ($_GET["user"]) {
 	$user = htmlspecialchars(pqkx_decrypt($user));
+
+	if ($_POST["getUserPublicKey"]) {
+		ob_clean();
+		$stmt = $mysqli->prepare("select keyvk, keyid from userkeys where username = ?");
+		$stmt->bind_param("s", $user);
+		$stmt->execute();
+		$result = $stmt->get_result()->fetch_row();
+		echo $result[0].";".$result[1];
+		return;
+	}
+	else if ($_POST["message"]) {
+	}
 }
 else {
 	$error = "User not specified";
@@ -33,7 +48,7 @@ else {
 
 		<form id="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>">
 			Message<br>
-			<textarea class="msgbox sensitive" type="text" name="message"></textarea><br><br>
+			<textarea class="msgbox sensitive" id="msgcontent" type="text" name="message"></textarea><br><br>
 
 			<input class="submit" type="submit" value="Send">
 		</form>
@@ -41,6 +56,22 @@ else {
 	<script>
 		document.querySelector("#homeBtn").onclick = () => {
 			window.location.href = "index.php";
+		};
+		document.querySelector("#form").onsubmit = () => {
+			let message = document.querySelector("#msgcontent").value;
+			document.querySelector("#msgcontent").value = "";
+
+			fetch("<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>?user=<?php echo htmlspecialchars($_GET["user"]); ?>", {
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded"
+				},
+				method: "POST",
+				body: "getUserPublicKey=true"
+			}).then(response => response.text())
+			.then(data => {
+				console.log(data);
+			});
+			return false;
 		};
 	</script>
 	<style>
@@ -52,7 +83,7 @@ else {
 			height: 50vh;
 		}
 		.submit {
-			width: 5vw;
+			width: 10vw;
 			height: 5vh;
 		}
 	</style>
