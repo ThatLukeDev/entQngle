98a94980945b29299fe55fccba522a7fe2882082
ThatLukeDev <luke@l2j.co.uk>
2025-06-13 15:12:44 +0100

    everything but base32

--- totp.php
+++ totp.php
@@ -18,7 +18,6 @@ function hmac_sha1($key, $text) { // key cannot be larger than size
 
 function hotp_6($key, $text) {
 	$mac = substr(hmac_sha1($key, $text), 0, 20);
-	echo bin2hex($mac) . "<br>";
 	$offset = ord($mac[19]) & 0xf;
 	return	  ((ord($mac[$offset]) & 0x7f) << 24
 		|  (ord($mac[$offset + 1])) << 16
@@ -26,18 +25,22 @@ function hotp_6($key, $text) {
 		|  (ord($mac[$offset + 3]))) % 100000000;
 }
 
-function totp($key, $t) {
+function totp($key) {
 	$btime = "";
 
-	$btime = 1;//intdiv(time(), $t);
+	$btime = intdiv(time(), 30);
 	$stime = "";
-	for ($i = 8; $i >= 0; $i--) {
+	for ($i = 7; $i >= 0; $i--) {
 		$stime .= chr(($btime >> ($i * 8)) & 0xff);
 	}
 
 	return hotp_6($key, $stime);
 }
 
-echo hotp_6("12345678901234567890", "\0\0\0\0\0\0\0\1");
+function genotpauth($user, $key) {
+	return "otpauth://totp/EntQngle:" . $user . "?digits=8&secret=" . $secret;
+}
+
+echo totp("12345678901234567890");
 
 ?>
