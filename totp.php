fba78ae499764a510d754d9f5b3134436c09c85b
ThatLukeDev <luke@l2j.co.uk>
2025-06-13 08:41:34 +0100

    fix hotp-6

--- totp.php
+++ totp.php
@@ -18,13 +18,26 @@ function hmac_sha1($key, $text) { // key cannot be larger than size
 
 function hotp_6($key, $text) {
 	$mac = substr(hmac_sha1($key, $text), 0, 20);
+	echo bin2hex($mac) . "<br>";
 	$offset = ord($mac[19]) & 0xf;
-	return	  (ord($mac[$offset]) & 0x7f) << 24
-		| (ord($mac[$offset + 1])) << 16
-		| (ord($mac[$offset + 2])) << 8
-		| (ord($mac[$offset + 3]));
+	return	  ((ord($mac[$offset]) & 0x7f) << 24
+		|  (ord($mac[$offset + 1])) << 16
+		|  (ord($mac[$offset + 2])) << 8
+		|  (ord($mac[$offset + 3]))) % 100000000;
 }
 
-echo hotp("abc", "def");
+function totp($key, $t) {
+	$btime = "";
+
+	$btime = 1;//intdiv(time(), $t);
+	$stime = "";
+	for ($i = 8; $i >= 0; $i--) {
+		$stime .= chr(($btime >> ($i * 8)) & 0xff);
+	}
+
+	return hotp_6($key, $stime);
+}
+
+echo hotp_6("12345678901234567890", "\0\0\0\0\0\0\0\1");
 
 ?>
